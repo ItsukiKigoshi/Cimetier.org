0887b91029e0e0ae7e5306c10a93420e2bc965fa
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-04-21 01:34:42 -0700

    rewrite tailwind ver

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon(), tailwind()],
+	integrations: [icon()],
 	site: "https://itsukikigoshi.gitlab.io",
 	base: "/cimetier.org",
 	outDir: "public",
 	publicDir: "static",
+	vite: {
+		plugins: [tailwind()],
+	},
 });
